de657e013e31c40e08bbe2b51ecb35eda8502347
Ahmed Irhain <meadsa2008@gmail.com>
2026-01-24 01:53:39 +0300

    fix: integrate async error handling in auth routes

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import express from "express";
 import cors from "cors";
 import errorHandler from "./middleware/ErrorHandler.js";
-import asyncErrorHandler from "./middleware/AsyncErrorHandler.js";
 import userRouter from "./routes/auth.route.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(errorHandler);
-app.use(asyncErrorHandler);
+
 app.use("/auth", userRouter);
 
 const PORT = process.env.PORT || 3000;
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import authController from '../controllers/auth.controller.js';
+import asyncerrorHandler from '../middleware/asyncErrorHandler.js';
 const router = express.Router();
 
-router.post('/login', authController.login);
-router.post('/signup', authController.signUp);
-
+router.post('/login', asyncerrorHandler(authController.login));
+router.post('/signup', asyncerrorHandler(authController.signUp));
 export default router;
