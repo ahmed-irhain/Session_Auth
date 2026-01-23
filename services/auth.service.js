09acb19093ec8dc23de590b46cbc85b272547b28
Ahmed Irhain <meadsa2008@gmail.com>
2026-01-24 02:46:36 +0300

    fix: update bcrypt import path and refactor file handling in auth.service.js

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,25 +1,32 @@
-import { hashPassword, comparePassword } from "../config/bcrypt.js";
+import { hashPassword, comparePassword } from "../utils/bcrypt.js";
 import database from "../Users.json" with { type: "json" };
-import { readFile, appendFile, writeFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const usersFile = join(__dirname, "../Users.json");
 
 export async function SignUp(name, email, password) {
   const hashedPassword = await hashPassword(password);
   const user = new Object({
     name: name,
     email: email,
-    password: hashedPassword,
+    password: hashedPassword
   });
   database.users.push(user);
 
   try {
-    await writeFile("../Users.json", JSON.stringify(database, null, 2), "utf8");
+    await writeFile(usersFile, JSON.stringify(database, null, 2), "utf8");
     return user;
   } catch (error) {
     throw new Error("Error saving user to database");
   }
 }
 export async function Login(email, password) {
-  const ReadUsers = await readFile("../Users.json", "utf8");
+  const ReadUsers = await readFile(usersFile, "utf8");
   const user = JSON.parse(ReadUsers).users.find((user) => user.email === email);
   const validate = await comparePassword(password, user.password);
   if (validate) return true;
