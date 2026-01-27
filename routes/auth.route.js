57e288e37c3ac283e8acd1ee130fb936b6cb00a7
Ahmed Irhain <meadsa2008@gmail.com>
2026-01-27 21:43:41 +0300

    fix: correct import casing for asyncErrorHandler in auth.route.js

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import authController from '../controllers/auth.controller.js';
-import asyncerrorHandler from '../middleware/asyncErrorHandler.js';
+import asyncerrorHandler from '../middleware/AsyncErrorHandler.js';
 const router = express.Router();
 
 router.post('/login', asyncerrorHandler(authController.login));
