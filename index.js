e7ea1e2b87533c2627876c4e301142095f722048
Ahmed Irhain <meadsa2008@gmail.com>
2026-03-15 02:54:03 +0300

    refactor: move error handling middleware into index.js

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 import express from "express";
 import cors from "cors";
-import errorHandler from "./middleware/ErrorHandler.js";
 import userRouter from "./routes/auth.route.js";
+import session from "express-session";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(errorHandler);
+app.use(session({
+  secret: '4020',
+  resave: false,
+  saveUninitialized: false, 
+}));
 
 app.use("/auth", userRouter);
 
 const PORT = process.env.PORT || 3000;
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.send(err.stack).status(err.status || 500)
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- middleware/ErrorHandler.js
+++
@@ -1,5 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(err.status || 500)
-}
-export default errorHandler;
