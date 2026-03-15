4ef00ddfca0f96421239f83f9a933fda3e795ed9
Ahmed Irhain <meadsa2008@gmail.com>
2026-03-15 03:16:52 +0300

    refactor: remove express-session library calling from index.js

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 import express from "express";
 import cors from "cors";
 import userRouter from "./routes/auth.route.js";
-import session from "express-session";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(session({
-  secret: '4020',
-  resave: false,
-  saveUninitialized: false, 
-}));
 
 app.use("/auth", userRouter);
 
