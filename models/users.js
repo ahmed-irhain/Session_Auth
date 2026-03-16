a736acf9c3ff14b0b57884cd8c2bdd0a796530fb
Ahmed Irhain <meadsa2008@gmail.com>
2026-03-16 08:56:51 +0300

    feat: implement AddUser function to insert new users into the database

--- models/users.js
+++ models/users.js
@@ -1,1 +1,5 @@
 import db from '../config/db.js';
+
+function AddUser(name, email, password){
+    db.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3)', [name, email, password])
+}
