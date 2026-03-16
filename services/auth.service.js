58ba536448178af36bf95297a7318997ba6d063e
Ahmed Irhain <meadsa2008@gmail.com>
2026-03-16 09:27:25 +0300

    refactor: streamline SignUp and Login functions to use users model instead of JSON file

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,35 +1,20 @@
 import { hashPassword, comparePassword } from "../utils/bcrypt.js";
-import database from "../Users.json" with { type: "json" };
-import { readFile, writeFile } from "fs/promises";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const usersFile = join(__dirname, "../Users.json");
+import users from "../models/users.js";
 
 export async function SignUp(name, email, password) {
   const hashedPassword = await hashPassword(password);
-  const user = new Object({
-    name: name,
-    email: email,
-    password: hashedPassword
-  });
-  database.users.push(user);
-
   try {
-    await writeFile(usersFile, JSON.stringify(database, null, 2), "utf8");
-    return user;
+    await users.AddUser(name, email, hashedPassword)
   } catch (error) {
     throw new Error("Error saving user to database");
   }
 }
-export async function Login(email, password) {
-  const ReadUsers = await readFile(usersFile, "utf8");
-  const user = JSON.parse(ReadUsers).users.find((user) => user.email === email);
-  const validate = await comparePassword(password, user.password);
-  if (validate) return true;
+
+export async function Login(email, passwordClaim) {
+  const user = await users.findByEmail(email)
+  const userPassword = user[0]["password"]
+  const validate = await comparePassword(passwordClaim, userPassword);
+  if (validate) return true; 
   else return false;
 }
 export default { SignUp, Login };
