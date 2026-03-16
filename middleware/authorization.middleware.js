0ed7c1021d8d8faa387e84b831aaa33ced322d66
Ahmed Irhain <meadsa2008@gmail.com>
2026-03-17 02:34:00 +0300

    feat: add CheckSession middleware for session validation

---
+++ middleware/authorization.middleware.js
@@ -0,0 +1,16 @@
+import session from "../models/session.js"
+
+function CheckSession (req, res, next){
+     const cookie=req.headers.cookie
+     const userSessionId = cookie.split('=')[1]
+    //  const userSessionId = cookie.match(/sessionId=([^;]+)/)[1]
+     const userSession = session.getSession(userSessionId)
+     if(userSession){
+        req.session = userSession;
+        next();
+     }
+     else{
+        res.status(401).send("Unable to find user session")
+     }
+
+}
