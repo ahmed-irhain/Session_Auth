60d765d0dfe11dbb8850c43775192c9addb9e293
Ahmed Irhain <meadsa2008@gmail.com>
2026-03-16 08:30:02 +0300

    fix: add port configuration

--- config/db.js
+++ config/db.js
@@ -4,7 +4,8 @@ const pool = new Pool({
     user: 'postgres',
     password: '2008',
     host: 'localhost',
-    database: 'session_auth'
+    database: 'session_auth',
+    port: 5432
 })
 
 export default pool;
