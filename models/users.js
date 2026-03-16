6d55f546914ace094058d68a3e1ad0103a578143
Ahmed Irhain <meadsa2008@gmail.com>
2026-03-16 09:02:52 +0300

    feat: add async findByEmail function to retrieve user by email

--- models/users.js
+++ models/users.js
@@ -3,3 +3,13 @@ import db from '../config/db.js';
 function AddUser(name, email, password){
     db.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3)', [name, email, password])
 }
+
+async function findByEmail(email){
+    try{
+        const result = await db.query('SELECT * FROM users WHERE email=$1', [email])
+        return result.rows
+
+    } catch(err){
+        throw err;
+    }
+}
